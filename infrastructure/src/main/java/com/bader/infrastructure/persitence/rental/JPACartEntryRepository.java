80bdd560b3651c659108b8a641980015ff14fef2
EnzoFilangi <enzo.filangi@efrei.net>
2023-01-10 16:23:05 +0100

    Changed ID type of CartEntry repository from Integer to UUID to make use of default repository methods

--- infrastructure/src/main/java/com/bader/infrastructure/persitence/rental/JPACartEntryRepository.java
+++ infrastructure/src/main/java/com/bader/infrastructure/persitence/rental/JPACartEntryRepository.java
@@ -3,6 +3,8 @@ package com.bader.infrastructure.persitence.rental;
 import com.bader.infrastructure.persitence.rental.entity.CartEntryEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface JPACartEntryRepository extends CrudRepository<CartEntryEntity, Integer> {
+import java.util.UUID;
+
+public interface JPACartEntryRepository extends CrudRepository<CartEntryEntity, UUID> {
 
 }
