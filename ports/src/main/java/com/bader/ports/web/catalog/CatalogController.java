8b9c4689471a73b105338f8d8dbf5c31b4d3b781
EnzoFilangi <enzo.filangi@efrei.net>
2023-01-10 15:09:54 +0100

    Made field private

--- ports/src/main/java/com/bader/ports/web/catalog/CatalogController.java
+++ ports/src/main/java/com/bader/ports/web/catalog/CatalogController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/catalog")
 public class CatalogController {
-    CatalogService catalogService;
+    private final CatalogService catalogService;
 
     CatalogController(CatalogService catalogService) {
         this.catalogService = catalogService;
