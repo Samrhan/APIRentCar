3a8e6da769aed002d7a04f429bfadc6effcc183b
EnzoFilangi <enzo.filangi@efrei.net>
2023-01-26 11:35:44 +0100

    Added comment explaining how to swap payment provider

--- infrastructure/src/main/java/com/bader/infrastructure/provider/payment/service/PaymentServiceConfiguration.java
+++ infrastructure/src/main/java/com/bader/infrastructure/provider/payment/service/PaymentServiceConfiguration.java
@@ -1,5 +1,6 @@
 package com.bader.infrastructure.provider.payment.service;
 
+import com.bader.domain.payment.provider.PaymentProvider;
 import com.bader.infrastructure.provider.payment.PaypalBasedPaymentProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +8,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PaymentServiceConfiguration {
     @Bean
-    public PaypalBasedPaymentProvider paypalBasedPaymentProvider(){
+    public PaymentProvider paymentProvider(){
+        /* We choose to use the PaypalBasedPaymentProvider now,
+           but this would be easy to change in the future : simply change this line to
+           instantiate a StripeBasedPaymentProvider instead, and the rest of the code doesn't need to change
+        */
         return new PaypalBasedPaymentProvider();
     }
 }
